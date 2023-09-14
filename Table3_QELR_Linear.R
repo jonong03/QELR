2cc10e51bbf5690abfc89f6f698ac0fc06e03b35
Jon Ong <jon.ong03@gmail.com>
2023-09-14 22:37:38 +0800

    Tidier codes. Also bug fixed for simdriver()

--- Table3_QELR_Linear.R
+++ Table3_QELR_Linear.R
@@ -1,7 +1,8 @@
 #Table3: Quadratic Exponential Logistic Regression (Linear)
+
 gc(); rm(list=ls())
 library(dplyr)
-library(data.table)
+#library(data.table)
 library(geepack)
 
 beta <- c(-2.4, -2.0, -2.6)
@@ -10,17 +11,15 @@ n_seq <- c(100,300,500)
 nmax <- max(n_seq)
 p <- 15
 
-rising <- function(n, V){
-  a = c(0,1)
-  m = dim(V)[1]
-  config0 = expand.grid(rep(list(a),m)) %>% as.matrix
+rqebd <- function(n, V){
+  m = ncol(V)
+  config0 = expand.grid(rep(list(c(0,1)),m)) %>% as.matrix
   
   pmf <- function(y) exp(sum((V%*%y)*y))
   pr <- sapply(1:nrow(config0), function(i) pmf(config0[i,]))
   cdf <- cumsum(pr)/sum(pr)
   Y <- sapply(1:n, function(x) config0[sum(cdf < runif(1)) + 1,])
   return(t(Y))
-  
 }
 
 datagen <- function(beta, gamma, n, p){
@@ -37,13 +36,12 @@ datagen <- function(beta, gamma, n, p){
     return(V)
   }
   
-  #U<- lapply(1:n, function(j) w_f())
   U<- lapply(1:n, function(i) lapply(1:ngamma, function(j) w_f()))
   
   for (i in 1:n){
     L <- lapply(1:ngamma, function(j) U[[i]][[j]] * gamma[j]/2) %>% Reduce(`+`,.)
     diag(L) <-  X[(i*p-p+1): (i*p),] %*% beta
-    Y[i,]<- rising(1, L)
+    Y[i,]<- rqebd(1, L)
   }
   # Design matrix
   y <- c(t(Y))
@@ -65,6 +63,7 @@ QELR <- function(DATA){
   col_name <- colnames(aa0)
   row_name <- rownames(aa0)
   rownames(aa0) <- paste0("glm_", row_name)
+  
   aa1 <- geeglm(y~.-1-id, family=binomial, id=id, 
                 data=DATA, corstr="independence") %>% summary %>% coef
   colnames(aa1) <- col_name
@@ -81,8 +80,8 @@ QELR <- function(DATA){
 }
 
 #One Run
-dat <- datagen(beta, gamma, n=max(n_seq), p)
-QELR(dat$DAT)
+#dat <- datagen(beta, gamma, n=max(n_seq), p)
+#QELR(dat$DAT)
 
 #Simulation
 simdriver<- function(iter, seedn, beta, gamma, p, n_seq){
@@ -102,22 +101,27 @@ simdriver<- function(iter, seedn, beta, gamma, p, n_seq){
   
   G <- OUT[1:iter,,,]
   se <- t(apply(G[,,1:np,1], c(2,3), sd))
-  glm_b <- t(apply(G[,,1:np,1], c(2,3), mean)) - true_par
-  glm_re <- t(apply(G[,,1:np,2], c(2,3), mean))/se
-  geei_b <- t(apply(G[,,1:np+np,1], c(2,3), mean)) - true_par
-  geei_re <- t(apply(G[,,1:np+np,2], c(2,3), mean))/se
-  geee_b <- t(apply(G[,,1:np+(np*2),1], c(2,3), mean)) - true_par
-  geee_re <- t(apply(G[,,1:np+(np*2),2], c(2,3), mean))/se
-  K <- cbind(c(se), c(glm_b), c(glm_re), c(geei_b), c(geei_re), c(geee_b), c(geee_re))
-  
-  colnames(K)<- c("SE", "GLM_Bias", "GLM_RE", "GEEIND_Bias","GEEIND_RE", "GEECS_Bias", "GEECS_Re")
-  rowname<- expand.grid(b= paste0("b",1:length(beta)-1),paste0("gamma",1:length(gamma)),n= paste0("n=",n_seq))
+  glm <- t(apply(G[,,1:np,1], c(2,3), mean))
+  glm_b <- glm - true_par
+  glm_se <- t(apply(G[,,1:np,2], c(2,3), mean))
+  glm_re <- glm_se/se
+  geeind <- t(apply(G[,,1:np+np,1], c(2,3), mean))
+  geeind_b <- geeind - true_par
+  geeind_se <- t(apply(G[,,1:np+np,2], c(2,3), mean))
+  geeind_re <- geeind_se/se
+  geeexc <- t(apply(G[,,1:np+(np*2),1], c(2,3), mean))
+  geeexc_b <- geeexc - true_par
+  geeexc_se <- t(apply(G[,,1:np+(np*2),2], c(2,3), mean))
+  geeexc_re <- geeexc_se/se
+  K <- cbind(c(se), c(glm), c(glm_b), c(glm_se), c(glm_re), c(geeind), c(geeind_b), c(geeind_se), c(geeind_re), c(geeexc), c(geeexc_b), c(geeexc_se), c(geeexc_re))
+  colnames(K)<- c("SE", "GLM","GLM_Bias", "GLM_SE","GLM R.E.", "GEEIND","GEEIND_Bias","GEEIND_SE","GEEIND R.E.", "GEEEXC","GEEEXC_Bias","GEEEXC_SE","GEEEXC R.E.")
+
+  rowname<- expand.grid(b= c(paste0("beta",1:length(beta)-1),paste0("gamma",1:length(gamma))),n= paste0("n=",n_seq))
   K<- cbind(rowname, Truth= rep(true_par, by= length(n_seq)), K)
 
 return(list(OUT= OUT, K= K))
 
 }
-sim2<- simdriver(iter=3, seedn=88, beta, gamma, p, n_seq)
+
+sim2<- simdriver(iter=10, seedn=88, beta, gamma, p, n_seq)
 sim2$K
-
-
